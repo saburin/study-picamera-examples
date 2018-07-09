ce0e7ee7d92a1aa5a7a6f57f102aa6924245cc40
saburin <syunsuke.f21@gmail.com>
2018-07-09 19:25:44 +0900

    Update main.py
    person_detectorの有効化

--- camera/main.py
+++ camera/main.py
@@ -4,7 +4,7 @@ from processor.simple_streamer import SimpleStreamer as VideoCamera
 # from processor.motion_detector import MotionDetector as VideoCamera
 # from processor.qr_detector import QRDetector as VideoCamera
 # from processor.face_detector import FaceDetector as VideoCamera
-# from processor.person_detector import PersonDetector as VideoCamera
+from processor.person_detector import PersonDetector as VideoCamera
 
 import time
 import threading
